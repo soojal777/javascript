6624cb7e389e673dec7d6ffeb0f053f78bf9ddee
soojal777 <singhsoojal@gmail.com>
2024-11-22 17:38:00 +0000

    type

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
-console.log("hello")
+// let a = 78
+// let b = "sgdg"
+// c = 67 //allowed
+// let bg;
+// console.table([a,b,c,bg]);
+// for(let i=0;i<4;i++)
+// {
+//     let a = i;
+// }
+// console.log(a);
+
+//datatypes->
+//null => standalone value
+//undefined => unassigned but declared
+//object
+
+// console.log(typeof null);//object
+// console.log(typeof undefined);//undefined
+
+// let v1 = null
+// let v2 = undefined
+// let v3 = "ab78"
+// let val1 = Number(v1)
+// let val2 = Number(v2)
+// let val3 = Number(v3)
+// console.log(val1)
+// console.log(val2)
+// console.log(val3)
+
+//""->false after type conversion to bool
+//"vdg"->true
